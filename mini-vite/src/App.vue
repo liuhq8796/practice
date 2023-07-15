ae6a534cc6c54d9eb544cf1fea6b863bb91cbae1
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2023-07-15 17:23:53 +0800

    feat: 实现 mini-vite

---
+++ mini-vite/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div><span>{{ msg }}</span></div>
+</template>
+
+<script>
+export default {
+    setup() {
+        const msg = 'Hello World'
+
+        return {
+            msg
+        }
+    }
+}
+</script>
